3c079eaa354b5439325c143ac74f02416b373e2b
Kontrazis Petros <55915800+petrosKon@users.noreply.github.com>
2021-02-25 09:35:56 +0100

    Added menu buttons

---
+++ Assets/Scripts/ChangeAnimation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+{
+    public ButtonConfigHelper buttonConfigHelper;
+    public int maximumSteps = 10;
+
+    private int stepsCounter = 1;
+    private int firstStep = 1;
+    
+    public void MoveToNext()
+    {
+        if(stepsCounter < maximumSteps)
+        {
+            stepsCounter++;
+        }
+        buttonConfigHelper.MainLabelText = stepsCounter.ToString() + "/" + maximumSteps.ToString();
+        Debug.Log("Pushed");
+    }
+
+    public void MoveToPrevious()
+    {
+        if(stepsCounter != firstStep)
+        {
+            stepsCounter--;
+        }
+        buttonConfigHelper.MainLabelText = stepsCounter.ToString() + "/" + maximumSteps.ToString();
+    }
+}
